c111245816c95be4df8cb23da42ff57b74734136
Sarah145 <sarahennis805@hotmail.com>
2021-01-29 16:59:59 +0000

    granja cell types

--- rscripts/pre_processing_granja.R
+++ rscripts/pre_processing_granja.R
@@ -6,20 +6,19 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(tibble)
-library(here)
 library(Matrix)
 library(stringr)
 
 # Start with scADT data
 # raw data available as rds file from GEO under accession GSE139369
 # only interested in healthy data so just downloaded GSM4138872, GSM4138873, GSM4138874, GSM4138875	
-# download link processed data is also available on their GitHub but that rds file only contains 6 proteins?
+# download link for processed data is also available on their GitHub but that rds file only contains 6 proteins?
 
 # read in data from GEO
-f1 <- t(readRDS(here('../data/granja/GSM4138872_scADT_BMMC_D1T1.rds')))
-f2 <- t(readRDS(here('../data/granja/GSM4138873_scADT_BMMC_D1T2.rds')))
-f3 <- t(readRDS(here('../data/granja/GSM4138874_scADT_CD34_D2T1.rds')))
-f4 <- t(readRDS(here('../data/granja/GSM4138875_scADT_CD34_D3T1.rds')))
+f1 <- t(readRDS('../data/granja/GSM4138872_scADT_BMMC_D1T1.rds'))
+f2 <- t(readRDS('../data/granja/GSM4138873_scADT_BMMC_D1T2.rds'))
+f3 <- t(readRDS('../data/granja/GSM4138874_scADT_CD34_D2T1.rds'))
+f4 <- t(readRDS('../data/granja/GSM4138875_scADT_CD34_D3T1.rds'))
 gran_adt <- data.frame(rbind(f1, f2, f3, f4))
 dim(gran_adt)
 
@@ -44,31 +43,37 @@ ggplot(pd, aes(x = value)) +
   theme_minimal(base_size = 14)
 
 # write out matrix of clr-transformed adt counts
-write.table(gran_clr, here('../data/granja/gran_adt_clr.csv'), sep = '\t', quote = F)
+write.table(gran_clr, '../data/granja/gran_adt_clr.csv', sep = '\t', quote = F)
 
 #-------
 
 # Now move on the scRNA data
-# I chose to use the rds that was available from their github because it has less cells which presumably means it's been QC-ed
-# download link for the rds file here: https://jeffgranja.s3.amazonaws.com/MPAL-10x/Supplementary_Data/Healthy-Data/scRNA-Healthy-Hematopoiesis-191120.rds
+# The rds that was available from their github was not right - possibly mislabelled cells? - so went with raw counts available from GEO
+# read in data from GEO
 
-gran_rna <- readRDS(here('../data/granja/scRNA-Healthy-Hematopoiesis-191120.rds'))
-
-# I only want bone marrow so will exclude PBMC samples
-gran_rna <- gran_rna[,!str_detect(colnames(gran_rna), 'PBMC')]
+f1 <- readRDS('../data/granja/GSM4138872_scRNA_BMMC_D1T1.rds')
+f2 <- readRDS('../data/granja/GSM4138873_scRNA_BMMC_D1T2.rds')
+f3 <- readRDS('../data/granja/GSM4138874_scRNA_CD34_D2T1.rds')
+f4 <- readRDS('../data/granja/GSM4138875_scRNA_CD34_D3T1.rds')
+gran_rna <- cbind(f1, f2, f3, f4)
+dim(gran_rna)
 
 # need to write out 3 separate files so I can read it in to python
 
 # write out counts matrix
-mat <- t(gran_rna@assays$data$counts)
-writeMM(mat, here('../data/granja/gran_rna.mtx'))
+writeMM(t(gran_rna), '../data/granja/gran_rna.mtx')
 
 # write out gene names
-gns <- data.frame(gene_symbol = colnames(mat))
-write.table(gns, here('../data/granja/gran_rna_genes.csv'), sep = '\t', quote = F, row.names = F)
+gns <- data.frame(gene_symbol = rownames(gran_rna))
+write.table(gns, '../data/granja/gran_rna_genes.csv', sep = '\t', quote = F, row.names = F)
 
 # write out barcode names and metadata
-meta <- data.frame(gran_rna@colData)
-write.table(meta, here('../data/granja/gran_rna_meta.csv'), sep = '\t', quote = F)
+# rds file on their GitHub has metadata for all cells - download link for the rds file here: https://jeffgranja.s3.amazonaws.com/MPAL-10x/Supplementary_Data/Healthy-Data/scRNA-Healthy-Hematopoiesis-191120.rds
+processed_gran <- readRDS('../data/granja/scRNA-Healthy-Hematopoiesis-191120.rds')
+meta <- data.frame(processed_gran@colData)
+meta$bc <- paste0(meta$Group, ':', meta$Barcode)
+rownames(meta) <- meta$bc
+meta <- meta[colnames(gran_rna), ]
+write.table(meta, '../data/granja/gran_rna_meta.csv', sep = '\t', quote = F)
 
 # I'll do the rest of the pre-processing in python ('../notebooks/pre_processing_granja.ipynb')
